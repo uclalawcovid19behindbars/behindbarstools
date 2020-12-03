b4db635a1a49451736f64e571afa68d9da9a7d39
hjohns12 <hopecaneel@gmail.com>
2020-12-03 16:46:14 -0500

    fix: update functions to avoid bugs

--- R/clean_facility_name.R
+++ R/clean_facility_name.R
@@ -4,29 +4,51 @@
 #' Cleans federal and non-federal facilities in separate processes, in order to use "State" to merge or not
 #'
 #' @param dat Scraped/historical data with columns Name and State, at the very least
+#' @param alt_name_xwalk Optional parameter provides an alternative facility name crosswalk
+#'
 #' @return data set with cleaned facility name column, "Name", from crosswalk on GitHub
 #'
 #' @import stringr
 #' @importFrom tidyr hoist
 #' @importFrom purrr pluck
 #' @importFrom purrr map
+#' @importFrom assertthat has_name
+#' @importFrom assertthat see_ifd
 #'
 #' @export
 
-clean_facility_name <- function(dat){
-  name_xwalk <- read_fac_spellings()
+clean_facility_name <- function(dat, alt_name_xwalk = FALSE){
+  if(alt_name_xwalk) {
+    name_xwalk <- alt_name_xwalk
+  }
+  else {
+    name_xwalk <- read_fac_spellings()
+  }
+  check_jurisdiction <- see_if(dat %has_name% "jurisdiction")
+  if(check_jurisdiction == FALSE){
+    dat$jurisdiction <- NA
+  } else {
+    dat$jurisdiction <- dat$jurisdiction
+  }
 
   dat <- dat %>%
-    mutate(scrape_name_clean = clean_fac_col_txt(Name, to_upper = TRUE))
+    mutate(scrape_name_clean = clean_fac_col_txt(Name, to_upper = TRUE),
+           federal_bool = case_when(
+             is_federal(jurisdiction) ~ TRUE,
+             is_federal(State) ~ TRUE,
+             is_federal(Facility) ~ TRUE,
+             TRUE ~ FALSE
+           ))
 
   nonfederal <- dat %>%
-    dplyr::filter_all(any_vars(!is_federal(.))) %>%
+    filter(federal_bool == FALSE) %>%
     nest_join(name_xwalk, by = c("scrape_name_clean" = "xwalk_name_raw",
                               "State" = "State")) %>%
     hoist(name_xwalk, Name = pluck("xwalk_name_clean", 1)) %>%
     mutate(Name = map(Name, first),
           Name = as.character(Name),
           Name = ifelse(is.na(Name), scrape_name_clean, Name))
+
   nrow_nonfederal <- nrow(nonfederal)
   if(nrow_nonfederal == 0) {nonfederal <- NULL}
 
@@ -34,7 +56,7 @@ clean_facility_name <- function(dat){
     dplyr::filter(is_federal(State))
 
   federal <- dat %>%
-    dplyr::filter_all(any_vars(is_federal(.))) %>%
+    filter(federal_bool == TRUE) %>%
     select(-State) %>%
     nest_join(name_xwalk,
               by = c("scrape_name_clean" = "xwalk_name_raw")) %>%
