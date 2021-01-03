254f54f54a450030e44b0778420922216af16e49
erika-tyagi <erika.tyagi@gmail.com>
2021-01-03 08:38:28 -0600

    Added theme for maps

--- R/theme_behindbars.R
+++ R/theme_behindbars.R
@@ -5,9 +5,9 @@
 #' @import ggplot2
 #' @export
 #'
-#' @param base_size base font size, given in pts 
+#' @param base_size base font size, given in pts
 #' @param base_family base font family
-#' 
+#'
 #' @examples
 #' library(ggplot2)
 #' iris %>%
@@ -30,28 +30,48 @@
 
 theme_behindbars <- function(
     base_size = 24, base_family = "Helvetica") {
-    
+
     ggplot2::theme_classic(
-        base_family = base_family, 
+        base_family = base_family,
         base_size = base_size
     ) +
         ggplot2::theme(
-            text =                element_text(color = "#555526"), 
-            strip.text =          element_text(color = '#555526'), 
-            axis.text =           element_text(color = "#555526"), 
+            text =                element_text(color = "#555526"),
+            strip.text =          element_text(color = '#555526'),
+            axis.text =           element_text(color = "#555526"),
             panel.grid.major.y =  element_line(color = "#92926C", linetype = "dotted"),
-            plot.title.position = "plot", 
-            plot.tag.position =   "bottomright", 
+            plot.title.position = "plot",
+            plot.tag.position =   "bottomright",
             axis.line.y =         element_blank(),
-            axis.ticks.y =        element_blank(), 
-            axis.title.x =        element_blank(), 
-            axis.line =           element_line(color = "#555526"), 
-            axis.ticks =          element_line(color = "#555526"), 
-            plot.caption =        element_text(margin = margin(t = 1.2 * base_size)), 
-            plot.subtitle =       element_text(margin = margin(b = 1.2 * base_size)), 
-            axis.title.y =        element_text(margin = margin(r = 1.2 * base_size)), 
+            axis.ticks.y =        element_blank(),
+            axis.title.x =        element_blank(),
+            axis.line =           element_line(color = "#555526"),
+            axis.ticks =          element_line(color = "#555526"),
+            plot.caption =        element_text(margin = margin(t = 1.2 * base_size)),
+            plot.subtitle =       element_text(margin = margin(b = 1.2 * base_size)),
+            axis.title.y =        element_text(margin = margin(r = 1.2 * base_size)),
             plot.tag =            element_text(size = base_size / 2, hjust = 0)
         )
+}
+
+theme_map_behindbars <- function(
+    base_size = 24, base_family = "Helvetica") {
+
+    behindbarstools::theme_behindbars(
+        base_size = base_size,
+        base_family = base_family
+    ) +
+        ggplot2::theme(
+            axis.line =           element_blank(),
+            axis.ticks =          element_blank(),
+            axis.text =           element_blank(),
+            axis.title =          element_blank(),
+            axis.title.y =        element_blank(),
+            panel.grid.major =    element_blank(),
+            panel.grid.minor =    element_blank(),
+            panel.grid.major.y =  element_blank(),
+            legend.position =     "top",
+            legend.title =        element_blank())
 }
 
 #' @import ggplot2
