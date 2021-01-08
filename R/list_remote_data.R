51dec429f01f67e3ce94ae31e668d45c14bb56f9
Neal Marquez <nmmarquez@gmail.com>
2021-01-08 16:33:10 -0500

    update: add date only feature to file listing func

--- R/list_remote_data.R
+++ R/list_remote_data.R
@@ -9,16 +9,19 @@
 #' "extracted_data"
 #' @param scraper_name character, a character string indicating a particular
 #' scraper to pull data for. Defaults to NULL which pulls all file locations
+#' @param dates_only logical, only return the dates rather than full file name.
+#' Should only be used when scraper_name is not null
 #'
 #' @return character vector of file location urls
 #'
 #' @examples
 #' list_remote_data("log_files")
 #' list_remote_data("raw_files", "lasd")
+#' list_remote_data("raw_files", "lasd", TRUE)
 #'
 #' @export
 
-list_remote_data <- function(folder, scraper_name = NULL){
+list_remote_data <- function(folder, scraper_name = NULL, dates_only = FALSE){
 
     folders <- c("raw_files", "log_files", "extracted_data")
 
@@ -39,6 +42,16 @@ list_remote_data <- function(folder, scraper_name = NULL){
     if(!is.null(scraper_name)){
         srch_str <- paste0("\\d+-\\d+-\\d+_", scraper_name, "\\..*")
         out_files <- out_files[stringr::str_ends(out_files, srch_str)]
+    }
+
+    if(is.null(scraper_name) & dates_only){
+        warning(
+            "dates_only parameter only should be used when ",
+            "scraper_name parameter is not null. Ignoring")
+    }
+
+    if(!is.null(scraper_name) & dates_only){
+        out_files <- unique(stringr::str_extract(out_files, "\\d+-\\d+-\\d+"))
     }
 
     return(out_files)
