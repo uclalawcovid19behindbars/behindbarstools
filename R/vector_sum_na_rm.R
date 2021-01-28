5225c2c22d5e040d513ddc239461e6cd2452f591
Neal Marquez <nmmarquez@gmail.com>
2021-01-28 16:43:56 -0500

    fix: vec_sum_na_rm

--- R/vector_sum_na_rm.R
+++ R/vector_sum_na_rm.R
@@ -4,20 +4,16 @@
 #' This function sums vectors together.
 #'
 #' @param ... numeric vectors of equal length
-#' @return numeric vector of length 1
+#' @return numeric vector of length equal to provided vectors
 #'
 #' @examples
-#' sum_na_rm(c(1:2, NA))
-#' sum_na_rm(c(NA, NA))
-#'
+#' tibble(x = c(NA, NA, NA), y = c(NA, 2, NA), z = c(1, NA, NA)) %>%
+#'    mutate(new = vector_sum_na_rm(x, y, z))
 #' @export
 
 vector_sum_na_rm <- function(...){
     d <- rbind(...)
     apply(rbind(...), 2, function(x){
-        if(all(is.na(x))){
-            NA
-        }
-        sum(x, na.rm = TRUE)
+        sum_na_rm(x)
     })
 }
